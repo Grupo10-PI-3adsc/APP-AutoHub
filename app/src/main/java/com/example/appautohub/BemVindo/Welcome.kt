87bbe02bfe3c3517b258ca1cb62394c585194163
carlosOsilva <carlos.osilva@sptech.school>
2025-03-15 19:55:06 -0300

    Tela inicial e de cadastro/login concluídas!

---
+++ app/src/main/java/com/example/appautohub/BemVindo/Welcome.kt
@@ -0,0 +1,119 @@
+package com.example.appautohub.BemVindo
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appautohub.R
+import com.example.appautohub.ui.theme.AppAutoHubTheme
+
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    val preto = Color(0xFF30323D)
+    val amarelo = Color(0xFFE8C547)
+    val branco = Color(0xFFFFF9FB)
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.imagem_cadastro), // Substitua pela sua imagem de fundo
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.lotus_fundo),
+                    contentDescription = null,
+                    modifier = Modifier.size(250.dp)
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+                    .background(branco)
+                    .padding(24.dp)
+                    .height(250.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Bem vindo!",
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(top = 16.dp, bottom = 10.dp)
+                    )
+
+                    Text(
+                        text = "Encontre o melhor autocenter para sua necessidade.",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(vertical = 8.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Button(
+                            onClick = { /* Navegar para Login */ },
+                            modifier = Modifier.weight(1f),
+                            colors = ButtonDefaults.buttonColors(containerColor = preto)
+                        ) {
+                            Text(text = "Login", fontSize = 16.sp, color = branco)
+                        }
+                        Button(
+                            onClick = { /* Navegar para Cadastro */ },
+                            modifier = Modifier.weight(1f),
+                            colors = ButtonDefaults.buttonColors(containerColor = amarelo)
+                        ) {
+                            Text(text = "Cadastro", fontSize = 16.sp, color = preto)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppAutoHubTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            WelcomeScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/appautohub/Cadastro/Register.kt
@@ -0,0 +1,140 @@
+package com.example.appautohub.Cadastro
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appautohub.R
+import com.example.appautohub.ui.theme.AppAutoHubTheme
+
+
+@Composable
+fun RegisterScreen(modifier: Modifier = Modifier) {
+    val preto = Color(0xFF30323D)
+    val cinza = Color(0xFF8B9EB7)
+    val amarelo = Color(0xFFE8C547)
+    val branco = Color(0xFFFFF9FB)
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.imagem_cadastro),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+                    .background(branco)
+                    .padding(24.dp)
+                    .height(500.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(
+                        text = "Cadastro",
+                        fontSize = 35.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 8.dp, bottom = 20.dp)
+                    )
+
+                    var nome by remember { mutableStateOf("") }
+                    var email by remember { mutableStateOf("") }
+                    var cpf by remember { mutableStateOf("") }
+                    var senha by remember { mutableStateOf("") }
+
+                    OutlinedTextField(
+                        value = nome,
+                        onValueChange = { nome = it },
+                        label = { Text("Nome") },
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+
+                    OutlinedTextField(
+                        value = cpf,
+                        onValueChange = { cpf = it },
+                        label = { Text("CPF") },
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("E-mail") },
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+
+                    OutlinedTextField(
+                        value = senha,
+                        onValueChange = { senha = it },
+                        label = { Text("Senha") },
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    // Botões
+                    Button(onClick = { /* Lógica de cadastro */ },
+                        modifier = Modifier.fillMaxWidth().height(60.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = amarelo)
+                    ){
+                        Text(text = "Fazer Cadastro", fontSize = 20.sp, color = preto)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppAutoHubTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            RegisterScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
